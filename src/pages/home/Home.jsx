b60e01c85db71bf4ca7867c676cdfaea36631fad
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-02-21 22:32:58 +0200

    The logic and CSS for card components have been added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import MainHeader from "../../components/MainHeader";
+import Programs from "../../components/Programs";
 import "./home.css";
 
 const Home = () => {
   return (
     <>
       <MainHeader />
+      <Programs />
     </>
   );
 };
