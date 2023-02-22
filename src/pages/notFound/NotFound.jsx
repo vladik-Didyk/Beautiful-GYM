c559d0aaf9ef1f0cdfb0c718c3c89199cbde1d0c
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-02-23 00:23:29 +0200

    The trainer page has been completed and styled

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 import "./notFound.css";
 
 const NotFound = () => {
-  return <section>
-    <div className="container notfound__container">
-      <h2>Page Not Found</h2>
-      <Link to="/" className="btn">Go Back Home</Link>
-    </div>
-  </section>;
+  return (
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">
+          Go Back Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,7 +1,41 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_5.jpg";
+import { trainers } from "../../data";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import Trainer from "../../components/Trainer";
 import "./trainers.css";
 
 const Trainers = () => {
-  return <div>Trainers</div>;
+  return (
+    <>
+      <Header title="Our Trainers" image={HeaderImage}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea facere
+        corporis reprehenderit quas cum labore vero deserunt tenetur iusto in,
+        blanditiis soluta placeat pariatur ipsa qui vel neque nam nulla.
+      </Header>
+
+      <section className="trainer-section">
+        <div className="container trainer-container">
+          {trainers?.map(({ id, image, name, job, socials }) => (
+            <Trainer
+              key={id}
+              image={image}
+              name={name}
+              job={job}
+              socials={[
+                { icon: <BsInstagram />, link: socials[0] },
+                { icon: <AiOutlineTwitter />, link: socials[1] },
+                { icon: <FaFacebookF />, link: socials[2] },
+                { icon: <FaLinkedinIn />, link: socials[3] },
+              ]}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Trainers;
