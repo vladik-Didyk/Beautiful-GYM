b95fb6952c001f02d14866c0d60a69bda9204065
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-02-22 12:36:43 +0200

    A new component "header" has been added to the project, as well as general styles

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,18 @@
-import './about.css'
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_1.jpg";
+import "./about.css";
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
-  )
-}
+    <>
+      <Header title="About Us" image={HeaderImage}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
+        vero architecto iusto officia non ut, atque in maiores officiis, rem
+        quas dicta, ipsa reprehenderit! Velit laboriosam maiores nostrum minima
+        saepe?
+      </Header>
+    </>
+  );
+};
 
-export default About
+export default About;
