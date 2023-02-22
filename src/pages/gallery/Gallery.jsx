900385474e7a5131462bda5befeaef0782d84dd7
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-02-22 23:34:39 +0200

    The planes page has been completed and styled

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,11 +1,36 @@
-import './gallery.css'
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_3.jpg";
+import "./gallery.css";
 
 const Gallery = () => {
-  return (
-    <div>
-      Gallery
-    </div>
-  )
-}
+  const galleryLength = 15;
+  const images = [];
 
-export default Gallery
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require("../../images/gallery" + i + ".jpg"));
+  }
+
+  return (
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod in qui
+        nobis non cum enim, labore error sed nam obcaecati optio doloremque.
+        Aut, sapiente reiciendis cupiditate quia deserunt expedita explicabo!
+      </Header>
+
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery ${index + 1}`} />
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
