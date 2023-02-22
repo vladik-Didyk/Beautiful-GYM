4c789eeb374767395892d6acf2594e542912904c
vladik-Didyk <vladik.didyk@GMAIL.COM>
2023-02-22 11:14:39 +0200

    The testimonials section has been updated with logic and CSS

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
 import Values from "../../components/Values";
 import FAQs from "../../components/FAQs";
+import Testimonials from "../../components/Testimonials";
 import "./home.css";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Programs />
       <Values />
       <FAQs />
+      <Testimonials />
     </>
   );
 };
